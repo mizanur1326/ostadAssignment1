36d879c099adb65075f66f6f4726d3079b1c6f7e
Mizanur Rahman <mizanur1326@gmail.com>
2024-05-18 19:58:20 +0600

    task1 done

--- string.php
+++ string.php
@@ -1,3 +1,9 @@
 <?php
+$strings = ["Hello", "World", "PHP", "Programming"];
 
-?>
+foreach ($strings as $str) {
+    $vowelCount = preg_match_all('/[aeiou]/i', $str);
+    $reversedStr = strrev($str);
+    echo "Original String: $str, Vowel Count: $vowelCount, Reversed String: $reversedStr \n" ;
+}
+
